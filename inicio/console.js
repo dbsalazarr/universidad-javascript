04d0a950e2b4b21c196284d8b21c433fc481d43c
dbsalazarr <dbsalazarr@gmail.com>
2024-06-05 18:41:50 -0500

    Hoisting en Javascript

--- inicio/console.js
+++ inicio/console.js
@@ -1,9 +1,26 @@
 console.log("Hola Mundo")
-console.log("Volviendo a Iniciar con JS")
+console.log("Volviendo a Iniciar con JS <3")
 
 let a, b, c;
 a = 10
-b = 23
+b = 45
 c = a + b
 c
+let nombre = "Bremdow D."
+let edad = true
+let flotante = null
 
+
+console.log(typeof a)
+console.log(typeof nombre)
+console.log(typeof edad)
+console.log(typeof flotante)
+
+// Aplicando el concepto de Hoisting
+/**
+ * Se define como la disponibilidad de las variables antes de sus declaración (en código) debido a que estas se inicializan primero al momento de ejecutar un código en JS
+*/
+x
+console.log(x) 
+
+//
