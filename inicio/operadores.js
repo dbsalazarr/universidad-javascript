476af0f3765ce33da0ad56363bca64a838fbe856
dbsalazarr <dbsalazarr@gmail.com>
2024-06-13 01:06:51 -0500

    Operadores en JS

---
+++ inicio/operadores.js
@@ -0,0 +1,10 @@
+let a, b, c
+
+a = 0
+// Pre-incremento: suma 1 en la misma línea de código
+++a
+console.log(a)
+
+// Post Incremento; suma 1 cuando se vuelve a usar la variable
+a++;
+console.log(a)
