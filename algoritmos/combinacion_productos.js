c6aa1e6fc3cbe94b1a691dc560625567f224de0e
bremdows <bremdows@gmail.com>
2024-06-28 12:43:38 -0500

    Sentencias de control en JS

---
+++ algoritmos/combinacion_productos.js
@@ -0,0 +1,7 @@
+console.log("Iniciando con JS desde fleet")
+listaProductos = [20, 10, 16, 23, 80, 120]
+precioTotal = 200
+
+listaProductos.forEach( (precio) => {
+    console.log(precio)
+})
---
+++ inicio/valores_logicos.js
@@ -0,0 +1,10 @@
+let a = true
+let b = false
+console.log(a)
+console.log(b)
+
+console.log("Se tienen que validar al resto de personas que quieren ejecutar algo con la realidad")
+
+if (a == !b) {
+    console.log("Ambos simbolos son iguales")
+}
